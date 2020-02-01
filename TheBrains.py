6be2717199c7f965478e0e0fccce3cf6d8bec00e
Patrick Kuzdzal <pkuzdzal@bu.edu>
2020-02-01 14:23:10 -0500

    camera test

---
+++ TheBrains.py
@@ -0,0 +1,28 @@
+import uuid
+import pyttsx3 as tts
+#from picamera import PiCamera
+from time import sleep
+
+def takePhoto():
+    cam = PiCamera()
+    camera.start_preview(alpha=192)
+    sleep(1)
+    hexID = generateID()
+    camera.capture("/home/pi/Desktop/Pictures/" + hexID)
+    camera.stop_preview()
+
+def generateID():
+    randID = uuid.uuid1()
+    return randID.hex
+
+def speak(obj):
+    client = tts.init()
+    getSpeed = client.getProperty('rate')
+    client.setProperty('rate', 145)
+    client.say(obj)
+    client.runAndWait()
+
+def main():
+    takePhoto
+
+main()
